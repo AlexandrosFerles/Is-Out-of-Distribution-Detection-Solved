8b70495aaa1bf7b24ff177dbb15f6cd1528076f6
ferles <ferles@kth.se>
2020-05-04 19:35:14 +0200

    local

--- src/scriptFiles/Custom/AK_custom_eb0.sh
+++ src/scriptFiles/Custom/AK_custom_eb0.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 conda activate isic
-python /Midgard/home/ferles/Dermatology/src/custom_train.py --c /Midgard/home/ferles/Dermatology/src/configs/Custom_ISIC/AK_exclude.json
+python /home/ferles/medusa/src/custom_train.py --c /home/ferles/medusa/src/configs/Custom_ISIC/AK_exclude.json
--- src/scriptFiles/Custom/BCC_custom_eb0.sh
+++ src/scriptFiles/Custom/BCC_custom_eb0.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 conda activate isic
-python /Midgard/home/ferles/Dermatology/src/custom_train.py --c /Midgard/home/ferles/Dermatology/src/configs/Custom_ISIC/BCC_exclude.json
+python /home/ferles/medusa/src/custom_train.py --c /home/ferles/medusa/src/configs/Custom_ISIC/BCC_exclude.json
