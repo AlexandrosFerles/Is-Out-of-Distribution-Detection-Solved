eac1b835564963e0fc482df593a266419d2d7ccb
ferles <ferles@kth.se>
2020-05-05 19:14:25 +0200

    local

---
+++ src/scriptFiles/ood/ISICvsDermofit/baseline.sh
@@ -0,0 +1,1 @@
+python /home/ferles/medusa/src/ood.py --ood_method baseline --num_classes 8 --in_distribution_dataset ISIC --out_distribution_dataset Dermofit-out --model_checkpoint /home/ferles/medusa/src/checkpoints/isic_classifiers/eb0Custom-best-balanced-accuracy-model_new.pth >> /home/ferles/medusa/src/results/txts/ISICvsDermoFit/In/baseline.txt
