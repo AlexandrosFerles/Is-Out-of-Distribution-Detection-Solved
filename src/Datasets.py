023abb672b3b97b74fc7451dd53f07752c8c315c
ferles <ferles@kth.se>
2020-05-08 01:04:52 +0200

    local

--- src/Datasets.py
+++ src/Datasets.py
@@ -14,6 +14,30 @@ np.random.seed(global_seed)
 torch.manual_seed(global_seed)
 torch.cuda.manual_seed(global_seed)
 
+
+# retrieved from https://stackoverflow.com/questions/55588201/pytorch-transforms-on-tensordataset
+class CustomTensorDataset(data.Dataset):
+    """TensorDataset with support of transforms.
+    """
+    def __init__(self, tensors, transform=None):
+        assert all(tensors[0].size(0) == tensor.size(0) for tensor in tensors)
+        self.tensors = tensors
+        self.transform = transform
+
+    def __getitem__(self, index):
+        x = self.tensors[0][index]
+
+        if self.transform:
+            x = self.transform(x)
+
+        y = self.tensors[1][index]
+
+        return x, y
+
+    def __len__(self):
+        return self.tensors[0].size(0)
+
+
 # TODO: MetaData
 class PandasDataSet(data.Dataset):
 
