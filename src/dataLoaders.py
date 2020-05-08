87fcf20b5db6d4257e01caac3715c2eb9a035301
ferles <ferles@kth.se>
2020-05-08 19:22:43 +0200

    local

--- src/dataLoaders.py
+++ src/dataLoaders.py
@@ -245,8 +245,8 @@ class OrderedCrops(object):
         cropped_image = img.crop((
                         crop_positions[0, 0]-self.crop_size/2,
                         crop_positions[0, 1]-self.crop_size/2,
-                        (crop_positions[0, 0]-height/2)+self.crop_size,
-                        (crop_positions[0, 1]-width/2) + self.crop_size
+                        (crop_positions[0, 0]-height/2)+self.crop_size/2,
+                        (crop_positions[0, 1]-width/2) + self.crop_size/2
                         ))
 
         temp = [cropped_image]
@@ -256,8 +256,8 @@ class OrderedCrops(object):
             cropped_image = img.crop((
                 crop_positions[i, 0]-self.crop_size/2,
                 crop_positions[i, 1]-self.crop_size/2,
-                (crop_positions[i, 0]-height/2)+self.crop_size,
-                (crop_positions[i, 1]-width/2) + self.crop_size
+                (crop_positions[i, 0]-height/2)+self.crop_size/2,
+                (crop_positions[i, 1]-width/2) + self.crop_size/2
                 ))
             temp.append(cropped_image)
 
