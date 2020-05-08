2669c87504757cbea1ec6a9aafd94af02fbe3692
ferles <ferles@kth.se>
2020-05-08 18:55:09 +0200

    local

--- src/dataLoaders.py
+++ src/dataLoaders.py
@@ -242,22 +242,22 @@ class OrderedCrops(object):
         if self.pad_if_needed and img.size[1] < self.crop_size:
             img = F.pad(img, (0, self.crop_size-img.size[1]))
 
-        ret_tuple = (   img.crop(
+        ret_tuple = (   img.crop((
                         crop_positions[0, 0]-height/2,
                         crop_positions[0, 1]-width/2,
                         (crop_positions[0, 0]-height/2)+height,
                         (crop_positions[0, 1]-width/2) + width
-                        )
+                        ))
                     )
 
         for i in range(1, self.ncrops):
 
-            temp_tuple = (   img.crop(
+            temp_tuple = (   img.crop((
                 crop_positions[i, 0]-height/2,
                 crop_positions[i, 1]-width/2,
                 (crop_positions[i, 0]-height/2)+height,
                 (crop_positions[i, 1]-width/2) + width
-                )
+                ))
             )
 
             ret_tuple = ret_tuple + temp_tuple
