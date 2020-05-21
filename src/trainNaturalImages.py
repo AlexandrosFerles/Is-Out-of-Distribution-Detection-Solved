0fc385347141ac978fe44d560b2083c9d710726d
ferles <ferles@kth.se>
2020-05-22 01:04:09 +0200

    local

--- src/trainNaturalImages.py
+++ src/trainNaturalImages.py
@@ -32,8 +32,6 @@ def train(args):
         flag = True
 
     model = build_model(args).to(device)
-    model._dropout = nn.Dropout(p=0.5)
-    model = model.to(device)
 
     dataset = args.dataset.lower()
 
