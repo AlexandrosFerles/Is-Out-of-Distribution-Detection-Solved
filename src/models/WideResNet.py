6be5dd2ae77ec1cbd2e9bd7688fa1770899b39ea
ferles <ferles@kth.se>
2020-05-22 00:23:58 +0200

    local

--- src/models/WideResNet.py
+++ src/models/WideResNet.py
@@ -141,16 +141,3 @@ class WideResNet(nn.Module):
                 return att1, att2, att3, self.fc(out)
             else:
                 return self.fc(out)
-
-
-if __name__ == '__main__':
-
-    # change d and k if you want to check a model other than WRN-40-2
-    d = 40
-    k = 2
-    strides = [1, 1, 2, 2]
-    net = WideResNet(d=d, k=k, n_classes=10, input_features=3, output_features=16, strides=strides)
-
-    # verify that an output is produced
-    sample_input = torch.ones(size=(1, 3, 32, 32), requires_grad=False)
-    net(sample_input)
