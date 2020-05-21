0f557ca77f0547cc7d8e6c647b839285fa62880a
ferles <ferles@kth.se>
2020-05-22 00:33:05 +0200

    local

--- src/trainNaturalImages.py
+++ src/trainNaturalImages.py
@@ -43,10 +43,10 @@ def train(args):
         optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, nesterov=True, weight_decay=5e-4)
         scheduler = MultiStepLR(optimizer, milestones=[20, 50, 80], gamma=0.2)
     else:
+        resize = True
         epochs = 40
         optimizer = optim.SGD(model.parameters(), lr=1.25e-2, momentum=0.9, nesterov=True, weight_decay=1e-4)
         scheduler = MultiStepLR(optimizer, milestones=[10, 20, 30], gamma=0.1)
-        resize = True
 
     if not flag:
         trainloader, val_loader, testloader = natural_image_loaders(dataset, train_batch_size=32, test_batch_size=32, validation_test_split=1000, save_to_pickle=True, resize=resize)
