e65dfa99fae6f27e8feaa32edf9d9257019809ea
ferles <ferles@kth.se>
2020-05-27 21:52:37 +0200

    local

--- src/trainFineGrainedEnsemble.py
+++ src/trainFineGrainedEnsemble.py
@@ -47,8 +47,8 @@ def train(args):
     else:
         pickle_files[0] = pickle_files[0].split(".pickle")[0]+f"_subset_{args.subset_index}.pickle"
         pickle_files[1] = pickle_files[1].split(".pickle")[0]+f"_subset_{args.subset_index}.pickle"
-        trainloader, val_loader, testloader = fine_grained_image_loaders_subset(dataset, subset_index=args.subset_index, validation_test_split=800, pickle_files=pickle_files)
         ipdb.set_trace()
+        trainloader, val_loader, testloader = fine_grained_image_loaders_subset(dataset, subset_index=args.subset_index, validation_test_split=800, pickle_files=pickle_files)
         train_ood_loader = fine_grained_image_loaders_subset(dataset, single=True, subset_index=args.subset_index, validation_test_split=800, pickle_files=pickle_files)
 
         if 'genOdin' in training_configurations.checkpoint:
