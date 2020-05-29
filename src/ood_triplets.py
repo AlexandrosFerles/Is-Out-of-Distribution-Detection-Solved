f3c8548680c6f0b00e485684e9f0971d6b895fb7
ferles <ferles@kth.se>
2020-05-29 22:20:11 +0200

    local

--- src/ood_triplets.py
+++ src/ood_triplets.py
@@ -596,9 +596,9 @@ if __name__ == '__main__':
         _odin(model, method_loaders, device, ind_dataset=ind_dataset, val_dataset=val_dataset, ood_datasets=all_datasets)
     elif ood_method == 'mahalanobis':
         _generate_Mahalanobis(model, loaders, device, ind_dataset=ind_dataset, val_dataset=val_dataset, ood_datasets=all_datasets, num_classes=args.num_classes)
-    # elif ood_method == 'self-supervision' or ood_method =='selfsupervision' or ood_method =='self_supervision' or ood_method =='rotation':
-    #     method_loaders = loaders[1:]
-    #     _rotation(model, method_loaders, ind_dataset=args.in_distribution_dataset, val_dataset=args.val_dataset, ood_dataset=args.out_distribution_dataset, num_classes=args.num_classes, exclude_class=args.exclude_class, device=device)
+    elif ood_method == 'self-supervision' or ood_method =='selfsupervision' or ood_method =='self_supervision' or ood_method =='rotation':
+        method_loaders = loaders[1:]
+        _rotation(model, method_loaders, device, ind_dataset=ind_dataset, val_dataset=val_dataset, ood_datasets=all_datasets, num_classes=args.num_classes)
     # elif ood_method == 'generalized-odin' or ood_method == 'generalizedodin':
     #     method_loaders = loaders[1:]
     #     _gen_odin_inference(model, method_loaders, device, ind_dataset=args.in_distribution_dataset, val_dataset=args.val_dataset, ood_dataset=args.out_distribution_dataset, exclude_class=args.exclude_class)
