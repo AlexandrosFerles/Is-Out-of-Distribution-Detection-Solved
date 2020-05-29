383ae349dd2faef7da27f3eea195f31e4366fef0
ferles <ferles@kth.se>
2020-05-29 19:38:49 +0200

    local

--- src/dataLoaders.py
+++ src/dataLoaders.py
@@ -1166,7 +1166,8 @@ def get_ood_loaders(ind_dataset, val_ood_dataset, test_ood_dataset, batch_size=3
     else:
         return [train_ind_loader, val_ind_loader, test_ind_loader, val_ind_loader, test_ood_loader]
 
-def get_triplets_loaders(ind_dataset, val_ood_dataset, ood_datasets, batch_size=32, dataset_size=1000):
+
+def get_triplets_loaders(ind_dataset, val_ood_dataset, ood_datasets, batch_size=32):
 
     _, transform_test = _get_image_transforms(ind_dataset, resize=True)
     ind_trainset, ind_testset = _get_dataset(ind_dataset, transforms=[transform_test, transform_test], test=True)
@@ -1181,7 +1182,7 @@ def get_triplets_loaders(ind_dataset, val_ood_dataset, ood_datasets, batch_size=
         test_ind_loader = DataLoader(ind_testset, batch_size=batch_size, num_workers=3)
 
     with open(f'val_indices_{val_ood_dataset}.pickle', 'rb') as val_val_pickle:
-        val_ood_trainset, _ = _get_dataset(ind_dataset, transforms=[transform_test, transform_test], test=True)
+        val_ood_trainset, _ = _get_dataset(val_ood_dataset, transforms=[transform_test, transform_test], test=True)
         val_valset_indices = pickle.load(val_val_pickle)
 
         val_sampler = SubsetRandomSampler(val_valset_indices)
