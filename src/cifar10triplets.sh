10aa2d6562f19252a0fdea8144b3dbac7c4ec804
ferles <ferles@kth.se>
2020-05-30 18:40:53 +0200

    local

--- src/cifar10triplets.sh
+++ src/cifar10triplets.sh
@@ -13,8 +13,8 @@ python ood_triplets.py --m mahalanobis --mc /raid/ferles/checkpoints/eb0/cifar10
 python ood_triplets.py --m rotation --mc /raid/ferles/checkpoints/eb0/cifar10/rot_eb0Cifar10.pth --nc 10 --in cifar10 --val stl  --dv 3 --bs 1 > results/rotation_cifar10val_stl.txt
 python ood_triplets.py --m generalizedodin --mc /raid/ferles/checkpoints/eb0/cifar10/genOdinCifar10.pth --nc 10 --in cifar10 --val stl  --dv 3 > results/genodin_cifar10val_stl.txt
 
-python ood_triplets.py --m baseline --mc /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10.pth --in cifar10 --nc 10 --val tiny  --dv 3 > results/baseline_cifar10val_tiny.txt
-python ood_triplets.py --m odin --mc /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10.pth --in cifar10 --nc 10 --val tiny  --dv 3 > results/odin_cifar10val_tiny.txt
-python ood_triplets.py --m mahalanobis --mc /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10.pth --nc 10 --in cifar10 --val tiny  --dv 3 --bs 10 > results/mahalanobis_cifar10val_tiny.txt
-python ood_triplets.py --m rotation --mc /raid/ferles/checkpoints/eb0/cifar10/rot_eb0Cifar10.pth --nc 10 --in cifar10 --val tiny  --dv 3 --bs 1 > results/rotation_cifar10val_tiny.txt
-python ood_triplets.py --m generalizedodin --mc /raid/ferles/checkpoints/eb0/cifar10/genOdinCifar10.pth --nc 10 --in cifar10 --val tiny  --dv 3 > results/genodin_cifar10val_tiny.txt
+python ood_triplets.py --m baseline --mc /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10.pth --in cifar10 --nc 10 --val tinyimagenet  --dv 3 > results/baseline_cifar10val_tinyimagenet.txt
+python ood_triplets.py --m odin --mc /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10.pth --in cifar10 --nc 10 --val tinyimagenet  --dv 3 > results/odin_cifar10val_tinyimagenet.txt
+python ood_triplets.py --m mahalanobis --mc /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10.pth --nc 10 --in cifar10 --val tinyimagenet  --dv 3 --bs 10 > results/mahalanobis_cifar10val_tinyimagenet.txt
+python ood_triplets.py --m rotation --mc /raid/ferles/checkpoints/eb0/cifar10/rot_eb0Cifar10.pth --nc 10 --in cifar10 --val tinyimagenet  --dv 3 --bs 1 > results/rotation_cifar10val_tinyimagenet.txt
+python ood_triplets.py --m generalizedodin --mc /raid/ferles/checkpoints/eb0/cifar10/genOdinCifar10.pth --nc 10 --in cifar10 --val tinyimagenet  --dv 3 > results/genodin_cifar10val_tinyimagenet.txt
--- src/svhntriplets.sh
+++ src/svhntriplets.sh
@@ -1,23 +1,23 @@
-python ood_triplets.py --m baseline --mc /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10.pth --in cifar10 --nc 10 --val cifar100  --dv 3 > results/baseline_cifar10val_cifar100.txt
-python ood_triplets.py --m odin --mc /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10.pth --in cifar10 --nc 10 --val cifar100  --dv 3 > results/odin_cifar10val_cifar100.txt
-python ood_triplets.py --m mahalanobis --mc /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10.pth --nc 10 --in cifar10 --val cifar100  --dv 3 --bs 10 > results/mahalanobis_cifar10val_cifar100.txt
-python ood_triplets.py --m rotation --mc /raid/ferles/checkpoints/eb0/cifar10/rot_eb0Cifar10.pth --nc 10 --in cifar10 --val cifar100  --dv 3 --bs 1 > results/rotation_cifar10val_cifar100.txt
-python ood_triplets.py --m generalizedodin --mc /raid/ferles/checkpoints/eb0/cifar10/genOdinCifar10.pth --nc 10 --in cifar10 --val cifar100  --dv 3 > results/genodin_cifar10val_cifar100.txt
+python ood_triplets.py --m baseline --mc /raid/ferles/checkpoints/eb0/svhn/eb0SVHN.pth --nc 10 --in svhn  --val cifar100  --dv 5 > results/baseline_svhnval_cifar100.txt
+python ood_triplets.py --m odin --mc /raid/ferles/checkpoints/eb0/svhn/eb0SVHN.pth --nc 10 --in svhn  --val cifar100  --dv 5 > results/odin_svhnval_cifar100.txt
+python ood_triplets.py --m mahalanobis --mc /raid/ferles/checkpoints/eb0/svhn/eb0SVHN.pth --nc 10 --in svhn --val cifar100  --dv 5 --bs 10 > results/mahalanobis_svhnval_cifar100.txt
+python ood_triplets.py --m rotation --mc /raid/ferles/checkpoints/eb0/svhn/rot_eb0SVHN.pth --nc 10 --in svhn --val cifar100  --dv 5 --bs 1 > results/rotation_svhn100val_cifar100.txt
+python ood_triplets.py --m generalizedodin --mc /raid/ferles/checkpoints/eb0/svhn/genOdinSVHN.pth --nc 10 --in svhn --val cifar100  --dv 5 > results/genodin_svhn100val_cifar100.txt
 
-python ood_triplets.py --m baseline --mc /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10.pth --in cifar10 --nc 10 --val svhn  --dv 3 > results/baseline_cifar10val_svhn.txt
-python ood_triplets.py --m odin --mc /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10.pth --in cifar10 --nc 10 --val svhn  --dv 3 > results/odin_cifar10val_svhn.txt
-python ood_triplets.py --m mahalanobis --mc /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10.pth --nc 10 --in cifar10 --val svhn  --dv 3 --bs 10 > results/mahalanobis_cifar10val_svhn.txt
-python ood_triplets.py --m rotation --mc /raid/ferles/checkpoints/eb0/cifar10/rot_eb0Cifar10.pth --nc 10 --in cifar10 --val svhn  --dv 3 --bs 1 > results/rotation_cifar10val_svhn.txt
-python ood_triplets.py --m generalizedodin --mc /raid/ferles/checkpoints/eb0/cifar10/genOdinCifar10.pth --in cifar10 --val svhn  --dv 3 > results/genodin_cifar10val_svhn.txt
+python ood_triplets.py --m baseline --mc /raid/ferles/checkpoints/eb0/svhn/eb0SVHN.pth --nc 10 --in svhn  --val cifar10  --dv 5 > results/baseline_svhnval_cifar10.txt
+python ood_triplets.py --m odin --mc /raid/ferles/checkpoints/eb0/svhn/eb0SVHN.pth --nc 10 --in svhn  --val cifar10  --dv 5 > results/odin_svhnval_cifar10.txt
+python ood_triplets.py --m mahalanobis --mc /raid/ferles/checkpoints/eb0/svhn/eb0SVHN.pth --nc 10 --in svhn --val cifar10  --dv 5 --bs 10 > results/mahalanobis_svhnval_cifar10.txt
+python ood_triplets.py --m rotation --mc /raid/ferles/checkpoints/eb0/svhn/rot_eb0SVHN.pth --nc 10 --in svhn --val cifar10  --dv 5 --bs 1 > results/rotation_svhn100val_cifar10.txt
+python ood_triplets.py --m generalizedodin --mc /raid/ferles/checkpoints/eb0/cifar10/genOdinCifar10.pth --nc 10 --in svhn --val cifar10  --dv 5 > results/genodin_svhn100val_cifar10.txt
 
-python ood_triplets.py --m baseline --mc /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10.pth --in cifar10 --nc 10 --val stl  --dv 3 > results/baseline_cifar10val_stl.txt
-python ood_triplets.py --m odin --mc /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10.pth --in cifar10 --nc 10 --val stl  --dv 3 > results/odin_cifar10val_stl.txt
-python ood_triplets.py --m mahalanobis --mc /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10.pth --nc 10 --in cifar10 --val stl  --dv 3 --bs 10 > results/mahalanobis_cifar10val_stl.txt
-python ood_triplets.py --m rotation --mc /raid/ferles/checkpoints/eb0/cifar10/rot_eb0Cifar10.pth --nc 10 --in cifar10 --val stl  --dv 3 --bs 1 > results/rotation_cifar10val_stl.txt
-python ood_triplets.py --m generalizedodin --mc /raid/ferles/checkpoints/eb0/cifar10/genOdinCifar10.pth --in cifar10 --val stl  --dv 3 > results/genodin_cifar10val_stl.txt
+python ood_triplets.py --m baseline --mc /raid/ferles/checkpoints/eb0/svhn/eb0SVHN.pth --nc 10 --in svhn  --val stl  --dv 5 > results/baseline_svhnval_stl.txt
+python ood_triplets.py --m odin --mc /raid/ferles/checkpoints/eb0/svhn/eb0SVHN.pth --nc 10 --in svhn  --val stl  --dv 5 > results/odin_svhnval_stl.txt
+python ood_triplets.py --m mahalanobis --mc /raid/ferles/checkpoints/eb0/svhn/eb0SVHN.pth --nc 10 --in svhn --val stl  --dv 5 --bs 10 > results/mahalanobis_svhnval_stl.txt
+python ood_triplets.py --m rotation --mc /raid/ferles/checkpoints/eb0/svhn/rot_eb0SVHN.pth --nc 10 --in svhn --val stl  --dv 5 --bs 1 > results/rotation_svhn100val_stl.txt
+python ood_triplets.py --m generalizedodin --mc /raid/ferles/checkpoints/eb0/cifar10/genOdinCifar10.pth --nc 10 --in svhn --val stl  --dv 5 > results/genodin_svhn100val_stl.txt
 
-python ood_triplets.py --m baseline --mc /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10.pth --in cifar10 --nc 10 --val tiny  --dv 3 > results/baseline_cifar10val_tiny.txt
-python ood_triplets.py --m odin --mc /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10.pth --in cifar10 --nc 10 --val tiny  --dv 3 > results/odin_cifar10val_tiny.txt
-python ood_triplets.py --m mahalanobis --mc /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10.pth --nc 10 --in cifar10 --val tiny  --dv 3 --bs 10 > results/mahalanobis_cifar10val_tiny.txt
-python ood_triplets.py --m rotation --mc /raid/ferles/checkpoints/eb0/cifar10/rot_eb0Cifar10.pth --nc 10 --in cifar10 --val tiny  --dv 3 --bs 1 > results/rotation_cifar10val_tiny.txt
-python ood_triplets.py --m generalizedodin --mc /raid/ferles/checkpoints/eb0/cifar10/genOdinCifar10.pth --in cifar10 --val tiny  --dv 3 > results/genodin_cifar10val_tiny.txt
+python ood_triplets.py --m baseline --mc /raid/ferles/checkpoints/eb0/svhn/eb0SVHN.pth --nc 10 --in svhn  --val tiny  --dv 5 > results/baseline_svhnval_tiny.txt
+python ood_triplets.py --m odin --mc /raid/ferles/checkpoints/eb0/svhn/eb0SVHN.pth --nc 10 --in svhn  --val tiny  --dv 5 > results/odin_svhnval_tiny.txt
+python ood_triplets.py --m mahalanobis --mc /raid/ferles/checkpoints/eb0/svhn/eb0SVHN.pth --nc 10 --in svhn --val tiny  --dv 5 --bs 10 > results/mahalanobis_svhnval_tiny.txt
+python ood_triplets.py --m rotation --mc /raid/ferles/checkpoints/eb0/svhn/rot_eb0SVHN.pth --nc 10 --in svhn --val tiny  --dv 5 --bs 1 > results/rotation_svhn100val_tiny.txt
+python ood_triplets.py --m generalizedodin --mc /raid/ferles/checkpoints/eb0/cifar10/genOdinCifar10.pth --nc 10 --in svhn --val tiny  --dv 5 > results/genodin_svhn100val_tiny.txt
