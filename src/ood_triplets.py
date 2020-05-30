c20faeaac30e384aded315f62a30e4c24c89eea8
ferles <ferles@kth.se>
2020-05-30 14:15:20 +0200

    local

--- src/ood_triplets.py
+++ src/ood_triplets.py
@@ -400,7 +400,6 @@ def _gen_odin_inference(model, loaders, device, ind_dataset, val_dataset, ood_da
     val_ind_loader, test_ind_loader, val_ood_loader, test_ood_loader_1, test_ood_loader_2, test_ood_loader_3 = loaders
 
     epsilons = [0, 0.0025, 0.005, 0.01, 0.02, 0.04, 0.08]
-    epsilons = [0]
 
     best_auc, best_epsilon = 0, 0
     for epsilon in epsilons:
