7caafb9cad335d4ce6347008f546f6fab9fd883d
ferles <ferles@kth.se>
2020-06-01 01:14:03 +0200

    local

--- src/trainFineGrained.py
+++ src/trainFineGrained.py
@@ -51,6 +51,7 @@ def train(args):
         else:
             pickle_files[0] = pickle_files[0].split(".pickle")[0]+f"_subset_{args.subset_index}.pickle"
             pickle_files[1] = pickle_files[1].split(".pickle")[0]+f"_subset_{args.subset_index}.pickle"
+            ipdb.set_trace()
             trainloader, val_loader, testloader, num_classes = fine_grained_image_loaders_subset(dataset, subset_index=args.subset_index, validation_test_split=800, pickle_files=None, ret_num_classes=True)
             print(num_classes)
 
