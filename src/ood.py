031370b8a8da8f6a14e7566d0e591808a75ab482
ferles <ferles@kth.se>
2020-05-31 03:18:03 +0200

    local

--- src/ood.py
+++ src/ood.py
@@ -389,8 +389,6 @@ def _odin(model, loaders, device, ind_dataset, val_dataset, ood_dataset, exclude
 
 def _generate_Mahalanobis(model, loaders, device, ind_dataset, val_dataset, ood_dataset, num_classes, exclude_class=None, model_type='eb0'):
 
-    ipdb.set_trace()
-
     model.eval()
     train_ind_loader, val_ind_loader, test_ind_loader, val_ood_loader, test_ood_loader = loaders
 
@@ -411,7 +409,7 @@ def _generate_Mahalanobis(model, loaders, device, ind_dataset, val_dataset, ood_
         feature_list[count] = out.size(1)
         count += 1
 
-    print(num_classes)
+    ipdb.set_trace()
     sample_mean, precision = lib_generation.sample_estimator(model, num_classes, feature_list, train_ind_loader, device=device)
 
     best_auc = 0
