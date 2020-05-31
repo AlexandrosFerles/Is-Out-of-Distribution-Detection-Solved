3a17588c46070101daad5660bd3739462e0683fa
ferles <ferles@kth.se>
2020-06-01 01:23:48 +0200

    local

--- src/trainFineGrainedEnsemble.py
+++ src/trainFineGrainedEnsemble.py
@@ -24,7 +24,7 @@ def train(args):
 
     json_options = json_file_to_pyobj(args.config)
     training_configurations = json_options.training
-    wandb.init(name=f"{training_configurations.checkpoint}_subset_{args.subset_index}_ensemble")
+    # wandb.init(name=f"{training_configurations.checkpoint}_subset_{args.subset_index}_ensemble")
     device = torch.device(f'cuda:{args.device}')
 
     flag = False
