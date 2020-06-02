e2091206bfad563c5a84a7a7756745264bbf93c3
ferles <ferles@kth.se>
2020-06-02 02:58:37 +0200

    local

--- src/rotationFineGrained.py
+++ src/rotationFineGrained.py
@@ -149,7 +149,7 @@ def train(args):
 
         wandb.log({'Test Set Accuracy': test_set_accuracy, 'epoch': epoch})
 
-        scheduler.step(epoch=epoch)
+        scheduler.step()
 
 
 if __name__ == '__main__':
