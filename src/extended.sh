10bb24e0c112082b5bc1f5f0bcb9622513766fe3
ferles <ferles@kth.se>
2020-06-04 02:05:29 +0200

    local

---
+++ src/extended.sh
@@ -0,0 +1,15 @@
+python ood_triplets.py --m baseline --mc /home/ferles/checkpoints/eb0/cifar10/extended_eb0Cifar10.pth --in cifar10 --val cifar100 --dv 1  --nc 10 > results/triplets/baseline_extended_cifar100.txt
+python ood_triplets.py --m odin --mc /home/ferles/checkpoints/eb0/cifar10/extended_eb0Cifar10.pth --in cifar10 --val cifar100 --dv 1  --nc 10 > results/triplets/odin_extended_cifar100.txt
+python ood_triplets.py --m mahalanobis --mc /home/ferles/checkpoints/eb0/cifar10/extended_eb0Cifar10.pth --in cifar10 --val cifar100 --bs 10 --dv 1  --nc 10 > results/triplets/mahalanobis_extended_cifar100.sh
+
+python ood_triplets.py --m baseline --mc /home/ferles/checkpoints/eb0/cifar10/extended_eb0Cifar10.pth --in cifar10 --val svhn --dv 1  --nc 10 > results/triplets/baseline_extended_svhn.txt
+python ood_triplets.py --m odin --mc /home/ferles/checkpoints/eb0/cifar10/extended_eb0Cifar10.pth --in cifar10 --val svhn --dv 1  --nc 10 > results/triplets/odin_extended_svhn.txt
+python ood_triplets.py --m mahalanobis --mc /home/ferles/checkpoints/eb0/cifar10/extended_eb0Cifar10.pth --in cifar10 --val svhn --bs 10 --dv 1  --nc 10 > results/triplets/mahalanobis_extended_svhn.sh
+
+python ood_triplets.py --m baseline --mc /home/ferles/checkpoints/eb0/cifar10/extended_eb0Cifar10.pth --in cifar10 --val stl --dv 1  --nc 10 > results/triplets/baseline_extended_stl.txt
+python ood_triplets.py --m odin --mc /home/ferles/checkpoints/eb0/cifar10/extended_eb0Cifar10.pth --in cifar10 --val stl --dv 1  --nc 10 > results/triplets/odin_extended_stl.txt
+python ood_triplets.py --m mahalanobis --mc /home/ferles/checkpoints/eb0/cifar10/extended_eb0Cifar10.pth --in cifar10 --val stl --bs 10 --dv 1  --nc 10 > results/triplets/mahalanobis_extended_stl.sh
+
+python ood_triplets.py --m baseline --mc /home/ferles/checkpoints/eb0/cifar10/extended_eb0Cifar10.pth --in cifar10 --val tinyimagenet --dv 1  --nc 10 > results/triplets/baseline_extended_tinyimagenet.txt
+python ood_triplets.py --m odin --mc /home/ferles/checkpoints/eb0/cifar10/extended_eb0Cifar10.pth --in cifar10 --val tinyimagenet --dv 1  --nc 10 > results/triplets/odin_extended_tinyimagenet.txt
+python ood_triplets.py --m mahalanobis --mc /home/ferles/checkpoints/eb0/cifar10/extended_eb0Cifar10.pth --in cifar10 --val tinyimagenet --bs 10 --dv 1  --nc 10 > results/triplets/mahalanobis_extended_tinyimagenet.sh
