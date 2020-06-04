38f19dee8d71520fff0e551f275c974d98893eef
ferles <ferles@kth.se>
2020-06-04 21:42:09 +0200

    local

--- src/isic_panelABC.sh
+++ src/isic_panelABC.sh
@@ -11,7 +11,7 @@ python ood.py --m rotation --mc /raid/ferles/checkpoints/isic_classifiers/rot_is
 python ood.py --m generalizedodin --mc /raid/ferles/checkpoints/isic_classifiers/GenOdinISICeb0-best-model.pth --in isic --val imagenet --out dermofit-in --nc 8 --dv 2  > results/isic/panelB/generalizedodin.txt
 
 python ood.py --m baseline --mc /raid/ferles/checkpoints/isic_classifiers/eb0CustomNoPreproc-best-model.pth --in isic --val imagenet --out dermofit-out --nc 8 --dv 2  > results/isic/panelC/baseline.txt
-python ood.py --m odin --mc /raid/ferles/checkpoints/isic_classifiers/eb0CustomNoPreproc-best-model.pth --out isic --val imagenet --out dermofit-out --nc 8 --dv 2  > results/isic/panelC/odin.txt
-python ood.py --m mahalanobis --mc /raid/ferles/checkpoints/isic_classifiers/eb0CustomNoPreproc-best-model.pth --out isic --val imagenet --out dermofit-out --nc 8 --dv 2  --bs 20  > results/isic/panelC/mahalanobis.txt
-python ood.py --m rotation --mc /raid/ferles/checkpoints/isic_classifiers/rot_isic-best-model.pth --out isic --val imagenet --out dermofit-out --nc 8 --dv 2  --bs 1 > results/isic/panelC/rotation.txt
-python ood.py --m generalizedodin --mc /raid/ferles/checkpoints/isic_classifiers/GenOdinISICeb0-best-model.pth --out isic --val imagenet --out dermofit-out --nc 8 --dv 2  > results/isic/panelC/generalizedodin.txt
+python ood.py --m odin --mc /raid/ferles/checkpoints/isic_classifiers/eb0CustomNoPreproc-best-model.pth --in isic --val imagenet --out dermofit-out --nc 8 --dv 2  > results/isic/panelC/odin.txt
+python ood.py --m mahalanobis --mc /raid/ferles/checkpoints/isic_classifiers/eb0CustomNoPreproc-best-model.pth --in isic --val imagenet --out dermofit-out --nc 8 --dv 2  --bs 20  > results/isic/panelC/mahalanobis.txt
+python ood.py --m rotation --mc /raid/ferles/checkpoints/isic_classifiers/rot_isic-best-model.pth --out isic --in imagenet --out dermofit-out --nc 8 --dv 2  --bs 1 > results/isic/panelC/rotation.txt
+python ood.py --m generalizedodin --mc /raid/ferles/checkpoints/isic_classifiers/GenOdinISICeb0-best-model.pth --in isic --val imagenet --out dermofit-out --nc 8 --dv 2  > results/isic/panelC/generalizedodin.txt
