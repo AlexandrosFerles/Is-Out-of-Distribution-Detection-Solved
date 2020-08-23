6d6b6959ee6c6ddab2de3ababb2f3975880d3aaa
ferles <ferles@kth.se>
2020-08-24 01:29:09 +0300

    local

--- src/sanity_gram_matrices.py
+++ src/sanity_gram_matrices.py
@@ -74,8 +74,11 @@ def _gram_matrices(model, loaders, device, num_classes, power=10):
         for c in range(num_classes):
             indices = np.where(argmaxs.detach().cpu().numpy() == c)
             for layer, feature_map in enumerate(features):
-                ipdb.set_trace()
-                selected_features = feature_map[indices[0]]
+                # ipdb.set_trace()
+                try:
+                    selected_features = feature_map[indices[0]]
+                except:
+                    ipdb.set_trace()
                 for p in (range(power)):
                     g_p = _get_gram_power(selected_features, p+1)
                     if g_p is not None:
