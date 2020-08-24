f907e9ed844d2921b9a24f7c106e454aeb6c3a9b
ferles <ferles@kth.se>
2020-08-24 04:40:19 +0300

    local

--- src/ablations1.sh
+++ src/ablations1.sh
@@ -1,3 +1,3 @@
-python ood_ensemble.py --in cifar10 --val cifar100 --test tinyimagenet-cifar10 --mcf /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10_accuracy_0.6129.pth --nc 10 --dv 7 > accuracy_0.6129*.txt
-python ood_ensemble.py --in cifar10 --val cifar100 --test tinyimagenet-cifar10 --mcf /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10_accuracy_0.7188.pth --nc 10 --dv 7 > accuracy_0.7188*.txt
-python ood_ensemble.py --in cifar10 --val cifar100 --test tinyimagenet-cifar10 --mcf /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10_accuracy_0.8273.pth --nc 10 --dv 7 > accuracy_0.8273*.txt
+python ood_ensemble.py --in cifar10 --val cifar100 --test tinyimagenet-cifar10 --mcf /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10_accuracy_0.6129.pth --nc 10 --dv 6 > accuracy_0.6129*.txt
+python ood_ensemble.py --in cifar10 --val cifar100 --test tinyimagenet-cifar10 --mcf /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10_accuracy_0.7188.pth --nc 10 --dv 6 > accuracy_0.7188*.txt
+python ood_ensemble.py --in cifar10 --val cifar100 --test tinyimagenet-cifar10 --mcf /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10_accuracy_0.8273.pth --nc 10 --dv 6 > accuracy_0.8273*.txt
