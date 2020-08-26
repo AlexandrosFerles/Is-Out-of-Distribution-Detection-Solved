3ee2ff398798daf814f96861a6ec4ce2a8c3bdaf
ferles <ferles@kth.se>
2020-08-26 19:25:42 +0300

    local

--- src/ablations.sh
+++
@@ -1,1 +0,0 @@
-python ood_ensemble.py --in cifar10 --val cifar100 --test tinyimagenet-cifar10 --mcf /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10_accuracy_0.2145.pth --nc 10 --dv 7 > accuracy_0.2145.txt
--- src/ablations1.sh
+++
@@ -1,2 +0,0 @@
-python ood_ensemble.py --in cifar10 --val cifar100 --test tinyimagenet-cifar10 --mcf /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10_accuracy_0.6129.pth --nc 10 --dv 6 > accuracy_0.6129*.txt
-python ood_ensemble.py --in cifar10 --val cifar100 --test tinyimagenet-cifar10 --mcf /raid/ferles/checkpoints/eb0/cifar10/eb0Cifar10_accuracy_0.7188.pth --nc 10 --dv 6 > accuracy_0.7188*.txt
